2bc328a66d63ecc73da44c41d0923984e281e562
phuang00 <phuang00@stuy.edu>
2019-03-15 11:22:57 -0400

    wrote wrapper function and started helper

--- Merge.java
+++ Merge.java
@@ -1,6 +1,10 @@
 public class Merge{
 
-  public static void mergesort(int[]data){
+  public static void mergesort(int[] data){
+    mergeH(data, 0, data.length);
+  }
+
+  private static void mergeH(int[] data, int lo, int hi){
     
   }
 
