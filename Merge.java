ebae422d8017021d986067acf1edfa9319793629
phuang00 <phuang00@stuy.edu>
2019-03-15 16:07:01 -0400

    simplified merge a little and tested it with Mr. K's driver

--- Merge.java
+++ Merge.java
@@ -32,7 +32,6 @@ public class Merge{
     int l = 0;
     int h = 0;
     for (int i = 0; i < data.length; i++){
-      if (!(l >= tempLo.length && h >= tempHi.length)){
         /*System.out.println("lo" + Arrays.toString(tempLo));
         System.out.println("hi" + Arrays.toString(tempHi));
         System.out.println("l" + l);
@@ -48,26 +47,49 @@ public class Merge{
           h++;
           //System.out.println("dataa" + Arrays.toString(data));
         }
-        else{
+        else if (h >= tempLo.length || (l < tempHi.length && tempLo[l] <= tempHi[h])){
           //System.out.println("BOOOO");
           data[i] = tempLo[l];
           l++;
         }
-      }
-      else{
-        i--;
-      }
+        else{
+          i--;
+        }
     }
   }
 
-  public static void main(String[] args) {
-    int[] ary = new int[] {3,5,2,6,31,6,23,2,54,64,25,4};
-    int[] arys = new int[] {3,5,2,6,31,6,23,2,54,64,25,4};
-    System.out.println(Arrays.toString(ary));
-    mergesort(ary);
-    System.out.println(Arrays.toString(ary));
-    Arrays.sort(arys);
-    System.out.println(Arrays.toString(arys));
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Merge.mergesort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
   }
-
 }
